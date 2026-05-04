aa3a2c950ca895ab894dcddbca23bd450e7384cc
Louis <louis.ginies@gmail.com>
2026-05-04 22:14:08 +0200

    Correction de ALLOWED_HOSTS et passage sur VS Code

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from website.views import home # Importe ta vue
 
 urlpatterns = [
-    path('admin/', admin.site.set.urls),
+    path('admin/', admin.site.urls),
     path('', home, name='home'), # L'adresse vide '' correspond à l'accueil
 ]
